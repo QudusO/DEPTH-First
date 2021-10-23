285f6606ebfbaa4833909483ae5a78c315cc24fb
Rahmzyking <92597729+Rahmzyking@users.noreply.github.com>
2021-10-23 11:36:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace dfs
+{
+    partial class Program
+    {
+        static Node sample_tree(){
+            Node root =
+                //new Node("A", new Node ("B",new Node("C"), new Node("D")),
+                //new Node("E", new Node("F"), new Node("G", new Node("H"), null)));
+
+                new Node("A", new Node("B", new Node("C"), new Node("D"), new Node("E"), new Node("6")),
+                          new Node("F"), new Node("G"), new Node("H"));
+            return root;
+        }
+        static void Main(string[] args)
+        {
+            Node tree = sample_tree();
+            Console.WriteLine("DFS-->");
+             dfs_traversal(tree);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Node{
+        public Node up, left, right, down; 
+        public String data;
+    
+
+        public Node(String data, Node up , Node left , Node right, Node down){
+            this.data = data;
+            this.up = up;
+            this.left = left;
+            this.right = right;
+            this.down = down;
+        }
+        public Node(String data){
+            this.data = data;
+            this.up = null;
+            this.left = null;
+            this.right = null;
+            this.down = null;
+        }
+        
+    
+        
+    
+    }
+
+}
---
+++ dfs_traversal.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;
+namespace dfs
+{
+    partial class Program{
+        static void dfs_traversal(Node node){
+            if(node==null){
+                return;
+            }else{
+                Console.Write(node.data + " ");
+            }
+            dfs_traversal(node.up);
+            dfs_traversal(node.left);
+            dfs_traversal(node.right);
+            dfs_traversal(node.down);
+        }
+    }
+}
